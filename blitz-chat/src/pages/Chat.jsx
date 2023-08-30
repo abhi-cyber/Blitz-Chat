61bd8a5afcf4fefca82f7abf3fe07e8e1adc7efd
abhi-cyber <abhirajchatrath@gmail.com>
2023-08-30 17:02:11 +0530

    Welcome screen

--- blitz-chat/src/pages/Chat.jsx
+++ blitz-chat/src/pages/Chat.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import {useNavigate} from "react-router-dom";
 import {allUsersRoute} from "../utils/APIRoutes";
 import Contacts from "../components/Contacts";
+import Welcome from "../components/Welcome";
 
 const Chat = () => {
   const navigate = useNavigate();
@@ -45,7 +46,12 @@ const Chat = () => {
   return (
     <Container>
       <div className="container">
-        <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange} />
+        <Contacts
+          contacts={contacts}
+          currentUser={currentUser}
+          changeChat={handleChatChange}
+        />
+        <Welcome currentUser={currentUser} />
       </div>
     </Container>
   );
