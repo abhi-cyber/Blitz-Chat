73cf3cdb91f6f295ed93aa48831b4b3514222298
abhi-cyber <abhirajchatrath@gmail.com>
2023-08-30 16:55:29 +0530

    All Contacts component

--- blitz-chat/src/pages/Chat.jsx
+++ blitz-chat/src/pages/Chat.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
+import styled from "styled-components";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
+import {allUsersRoute} from "../utils/APIRoutes";
+import Contacts from "../components/Contacts";
 
 const Chat = () => {
-  return (
-    <div>Chat</div>
-  )
-}
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  const [currentChat, setCurrentChat] = useState(undefined);
 
-export default Chat
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!localStorage.getItem("chat-app-user")) {
+        navigate("/login");
+      } else {
+        setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchContacts = async () => {
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          const response = await axios.get(
+            `${allUsersRoute}/${currentUser._id}`
+          );
+          setContacts(response.data);
+        } else {
+          navigate("/setAvatar");
+        }
+      }
+    };
+    fetchContacts();
+  }, [currentUser]);
+
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
+
+  return (
+    <Container>
+      <div className="container">
+        <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange} />
+      </div>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+    @media screen and (min-width: 360px) and (max-width: 480px) {
+      grid-template-columns: 50% 50%;
+    }
+  }
+`;
+
+export default Chat;
